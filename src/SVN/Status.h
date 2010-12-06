60752f7d620560e9c33b1b408d60d73139d19ce5
skonno <skonno@e17c22ee-00a2-4bc3-ab0a-07fc709df707>
2010-12-06 14:32:09 +0000

    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/cgsvnobjc/trunk@24 e17c22ee-00a2-4bc3-ab0a-07fc709df707

---
+++ src/SVN/Status.h
@@ -0,0 +1,36 @@
+//
+//  Status.h
+//  iSubversion
+//
+//  Created by Satoshi Konno on 10/12/06.
+//  Copyright 2010 Yahoo Japan Corporation. All rights reserved.
+//
+
+#import <SVN/Core.h>
+
+enum {
+	CGSvnStatusUnknown = 0,
+	CGSvnStatusNone,
+	CGSvnStatusUnversioned,
+	CGSvnStatusNormal,
+	CGSvnStatusAdded,
+	CGSvnStatusMissing,
+	CGSvnStatusDeleted,
+	CGSvnStatusReplaced,
+	CGSvnStatusModified,
+	CGSvnStatusMerged,
+	CGSvnStatusConflicted,
+	CGSvnStatusIgnored,
+	CGSvnStatusObstructed,
+	CGSvnStatusExternal,
+	CGSvnStatusIncomplete,
+};
+
+
+@interface Status : Core {
+
+}
+@property(retain) NSString *path;
+- (int)textStatus;
+- (int)propStatus;
+@end
---
+++ src/SVN/Status.m
@@ -0,0 +1,84 @@
+//
+//  Status.m
+//  iSubversion
+//
+//  Created by Satoshi Konno on 10/12/06.
+//  Copyright 2010 Yahoo Japan Corporation. All rights reserved.
+//
+
+#import "SVN/Status.h"
+#include <svn_wc.h>
+
+@implementation Status
+
+@synthesize path;
+
+- (id)initWithCObject:(const void *)cObj
+{
+	if (self = [super initWithCObject:cObj]) {
+		svn_wc_status_t *statusObj = (svn_wc_status_t *)[self cObject];
+		svn_wc_status_t *statusCopyObj = malloc(sizeof(svn_wc_status_t));
+		*statusCopyObj = *statusObj;
+		[self setCObject:statusCopyObj];
+	}
+	return self;
+}
+
+-(void)dealloc
+{
+	[super dealloc];
+
+	svn_wc_status_t *statusObj = (svn_wc_status_t *)[self cObject];
+	if (statusObj)
+		free(statusObj);
+}
+
+- (int)statusKindFrom:(int)statusKind
+{
+	switch (statusKind) {
+		case svn_wc_status_none:
+			return CGSvnStatusNone;
+		case svn_wc_status_unversioned:
+			return CGSvnStatusUnversioned;
+		case svn_wc_status_normal:
+			return CGSvnStatusNormal;
+		case svn_wc_status_added:
+			return CGSvnStatusAdded;
+		case svn_wc_status_missing:
+			return CGSvnStatusMissing;
+		case svn_wc_status_deleted:
+			return CGSvnStatusDeleted;
+		case svn_wc_status_replaced:
+			return CGSvnStatusReplaced;
+		case svn_wc_status_modified:
+			return CGSvnStatusModified;
+		case svn_wc_status_merged:
+			return CGSvnStatusMerged;
+		case svn_wc_status_conflicted:
+			return CGSvnStatusConflicted;
+		case svn_wc_status_ignored:
+			return CGSvnStatusIgnored;
+		case svn_wc_status_obstructed:
+			return CGSvnStatusObstructed;
+		case svn_wc_status_external:
+			return CGSvnStatusExternal;
+		case svn_wc_status_incomplete:
+			return CGSvnStatusIncomplete;
+	}
+	
+	return CGSvnStatusUnknown; 	
+}
+
+- (int)textStatus
+{
+	svn_wc_status_t *statusObj = (svn_wc_status_t *)[self cObject];
+	return [self statusKindFrom:statusObj->text_status];
+}
+
+- (int)propStatus
+{
+	svn_wc_status_t *statusObj = (svn_wc_status_t *)[self cObject];
+	return [self statusKindFrom:statusObj->prop_status];
+}
+
+@end
